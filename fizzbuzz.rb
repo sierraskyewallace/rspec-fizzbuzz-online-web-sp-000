1a203fff701d9849ea2426408e3832aaa0b6cb36
Sierra Wallace <sskwallace@gmail.com>
2020-07-21 06:50:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0
-    int = "Fizz"
- elsif int % 3 == 0 && int % 5 == 0
+  if int % 3 == 0 && int % 5 == 0
   int = "FizzBuzz"
- elsif int % 5 == 0
+  elsif int % 3 == 0
+    int = "Fizz"
+  elsif int % 5 == 0
   int = "Buzz"
  else int
    nil
