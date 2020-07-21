9cafce3dfb65b3f15b001b19aacd064943d7d875
Sierra Wallace <sskwallace@gmail.com>
2020-07-21 06:34:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 def fizzbuzz(int)
   if int  3 == 0 && int 5 == 0
-    puts "Fizzbuzz"
-   elsif int 5 == 0
-    puts "Buzz"
-   elsif int 3 == 0
-    puts "Fizz"
+    "Fizzbuzz"
+    elsif int 5 == 0
+    "Buzz"
+    elsif int 3 == 0
+     "Fizz"
   else
     puts "nil"
   end
