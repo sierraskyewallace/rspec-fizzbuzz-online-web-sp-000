3bf7f00737f3e4f8005cbc080b43c4a96af24cbf
Sierra Wallace <sskwallace@gmail.com>
2020-07-21 06:45:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,11 @@
 def fizzbuzz(i)
   if i % 3 == 0
     puts "Fizz"
-  elsif i % 3 == 0 && i % 5 == 0
-    puts "FizzBuzz"
-  elsif i % 5 == 0
-    puts "Buzz"
-  else nil
-end
+ elsif i % 3 == 0 && i % 5 == 0
+ puts "FizzBuzz"
+ elsif i % 5 == 0
+ puts "Buzz"
+ else nil
+   
+ end
 end
